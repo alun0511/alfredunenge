a3bbb62b6f18a517c2dc5ad931b83a185a151eba
alun <unenge.alfred@gmail.com>
2026-05-13 22:45:10 +0200

    Boilerplate html, css and js.

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('action-btn');
+    
+    btn.addEventListener('click', () => {
+        alert('Kul att du besöker min self-hostade server!');
+        console.log('Knappen klickades - JavaScript fungerar!');
+    });
+});
